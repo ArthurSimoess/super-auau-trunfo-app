83d7eda52d24718b4f0e10c8513d6c5c9aede412
Arthur Simões <arthursimoes155@gmail.com>
2021-12-26 17:02:05 -0300

    add instruction page

--- src/Routes.js
+++ src/Routes.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import InstructionsPage from "./pages/InstructionsPage";
 
 function Routes() {
   return(
     <Switch>
       <Route exact path="/" component={ HomePage }/>
+      <Route exact path="/instructions" component={ InstructionsPage } />
     </Switch>
   )
 }
