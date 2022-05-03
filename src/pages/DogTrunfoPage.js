debfe4989c90cd9e9f981e1da3b2d8de6b72a70e
Arthur Simões <arthursimoes155@gmail.com>
2022-05-03 00:39:32 -0300

    Fix: mobile responsivity

--- src/pages/DogTrunfoPage.js
+++ src/pages/DogTrunfoPage.js
@@ -4,7 +4,7 @@ import sortDeckdog from '../data/DogTrunfo';
 
 function DogTrunfoPage() {
   return (
-    <div className="bg-container h-screen overflow-auto">
+    <div className="bg-container h-screen overflow-auto md:flex justify-center items-center">
       <RenderGame
         deck={sortDeckdog}
       />
